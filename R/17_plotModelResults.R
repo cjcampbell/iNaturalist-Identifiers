22ebd5c12ebe4a7bf08b9cd9598f5a649ae0e094
Caitlin J Campbell <cjcampbell@users.noreply.github.com>
2023-08-08 13:47:46 -0400

    Update 17_plotModelResults.R
    Remove (commented out) example code

--- R/17_plotModelResults.R
+++ R/17_plotModelResults.R
@@ -497,17 +497,6 @@ whoLeft_count2 <- whoLeft %>%
 # Load package
 library(networkD3)
 
-# # Load energy projection data
-# URL <- "https://cdn.rawgit.com/christophergandrud/networkD3/master/JSONdata/energy.json"
-# Energy <- jsonlite::fromJSON(URL)
-# 
-# # Now we have 2 data frames: a 'links' data frame with 3 columns (from, to, value), and a 'nodes' data frame that gives the name of each node.
-# 
-# # Thus we can plot it
-# sankeyNetwork(Links = Energy$links, Nodes = Energy$nodes, Source = "source",
-#               Target = "target", Value = "value", NodeID = "name",
-#               units = "TWh", fontSize = 12, nodeWidth = 30)
-
 qg<-list()
 qg$nodes <- data.frame(name=c(
   "Start",          
